13a4950c28d9c6a7feeac7cbb8f7a041bc8f3baf
Samuel Kiarie <skiarie21@gmail.com>
2023-06-08 18:17:57 -0700

    a program that imports the function def add(a, b):

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
-from add_0 import add
+if __name__ == "__main__":
+    from add_0 import add
 
-a = 1
-b = 2
+    a = 1
+    b = 2
 
-add_result = add(a, b)
-print("{} + {} = {}".format(a, b, add_result))
+    add_result = add(a, b)
+    print("{} + {} = {}".format(a, b, add_result))
