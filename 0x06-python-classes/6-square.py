0256db78782a38082980b19c36de3bef29303040
Samuel Kiarie <skiarie21@gmail.com>
2023-06-27 19:25:32 -0700

    class Square that defines a square by: (based on 5-square.py

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -11,8 +11,10 @@ class Square:
 
         Args:
             size (int): size of square
+            position (tuple): position of square
         """
         self.size = size
+        self.position = position
 
     @property
     def size(self):
@@ -39,15 +41,37 @@ class Square:
         else:
             self.__size = value
 
+    @property
+    def position(self):
+        """obtain then return tuple position of square"""
+        return self.__position
+
+    @position.setter
+    def position(self, value):
+        """set position of square
+
+        Args:
+            value: poition of square tuple
+
+        Raises:
+            TypeError: if position is not tuple of 2 positive integers
+        """
+        if not isinstance(value, tuple) or len(value) != 2 \
+                or not all(isinstance(num, int) for num in value) \
+                or not all(num >= 0 for num in value):
+            raise TypeError("position must be a tuple of 2 positive integers")
+        self.__position = value
+
     def area(self):
         """Returns calculated area of the square."""
         return (self.__size ** 2)
 
     def my_print(self):
         """print the square with # character"""
-        if self.size == 0:
+        if self.__size == 0:
             print()
         else:
-            for i in range(self.size):
-                {print("#", end="") for j in range(self.__size)}
+            for i in range(self.__position[1]):
                 print()
+            for j in range(self.__size):
+                print(" ", * self.__position[0] + "#" * self.__size)
