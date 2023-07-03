ffc3974b9570846d903904be151bfddbc4fece5e
Samuel Kiarie <skiarie21@gmail.com>
2023-07-02 20:36:07 -0700

    function that prints the last digit of a number

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
 def uppercase(str):
+    result = ""
     tmp_str = list(str)
     for i in range(len(tmp_str)):
         if ord(tmp_str[i]) >= 97 and ord(tmp_str[i]) <= 122:
-            tmp_str[i] = chr(ord(tmp_str[i]) - 32)
-    print("{}".format("".join(tmp_str[i])))
+            result += chr(ord(tmp_str[i]) - 32)
+        else:
+            result += tmp_str[i]
+    print(result)
