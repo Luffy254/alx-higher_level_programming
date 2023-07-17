edc3edf20e318af63492f00b196be924307a58da
Samuel Kiarie <87488083+Luffy254@users.noreply.github.com>
2023-07-17 18:46:14 +0300

    Delete 0x0B-python-input_output directory

--- 0x0B-python-input_output/3-to_json_string.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-
-"""Module defines a string-to-JSON function"""
-import json
-
-
-def to_json_string(my_obj):
-    """Returns the JSON representation of an object(string)"""
-    return json.dumps(my_obj)
--- 0x0B-python-input_output/4-from_json_string.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-
-"""Module defines a JSON-to-object function"""
-import json
-
-
-def from_json_string(my_str):
-    """function that returns an object represented by a JSON string"""
-    return json.loads(my_str)
