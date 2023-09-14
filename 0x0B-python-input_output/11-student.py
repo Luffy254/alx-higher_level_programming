b4d750b572fe66583713760bddb93332dee1cb96
Samuel Kiarie <skiarie21@gmail.com>
2023-09-13 19:59:00 -0700

    class Student that defines a student by: (based on 10-student.py

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+
+"""Module defines a class Student"""
+
+
+class Student:
+    """Represents a student"""
+
+    def __init__(self, first_name, last_name, age):
+        """Initializes a new Student
+
+         Args:
+            first_name (str): first name of the student
+            last_name (str): last name of the student
+            age (int): age of the student
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Retrieves a dictionary representation of a Student
+        If attrs is a list of strings, retrieve only those attributes
+        included in the list.
+        Args:
+            attrs (list): (not a must) the attributes to retrieved.
+        """
+        if (type(attrs) == list and
+                all(type(ele) == str for ele in attrs)):
+            return {i: getattr(self, i) for i in attrs if hasattr(self, i)}
+        return self.__dict_
+
+    def reload_from_json(self, json):
+        """Replaces all attributes of the Student instance
+        Args:
+            json (dict): key/value pairs to replacing attributes.
+        """
+        for i, j in json.items():
+            setattr(self, i, j)
