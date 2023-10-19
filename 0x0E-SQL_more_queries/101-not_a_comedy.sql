dbb0098b74d4b4dabe2db99eefa2e465dc79c9f8
Samuel Kiarie <skiarie21@gmail.com>
2023-10-18 19:45:39 -0700

    lists all shows without the genre Comedy in the database hbtn_0d_tvshows

---
+++ 0x0E-SQL_more_queries/101-not_a_comedy.sql
@@ -0,0 +1,6 @@
+-- lists all shows without the genre Comedy in the database hbtn_0d_tvshows
+SELECT g.name
+FROM tv_genres AS g
+JOIN tv_show_genres AS sg ON g.id = sg.genre_id
+JOIN tv_shows AS s ON s.id = sg.show_id
+WHERE g.name <> 'Comedy';
