8d97147f26ae10e1eeabd4803215119e1f2139b6
Samuel Kiarie <skiarie21@gmail.com>
2023-10-18 19:37:08 -0700

    Import the database dump from hbtn_0d_tvshows to your MySQL server

--- 0x0E-SQL_more_queries/16-shows_by_genre.sql
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -1,6 +1,6 @@
 -- lists all shows, and all genres linked to that show, from the database hbtn_0d_tvshows
 SELECT s.title AS show_title, g.name AS genre
-FROM tv_shows s
-LEFT JOIN tv_show_genres sg ON s.id = sg.show_id
-LEFT JOIN tv_genres g ON g.id = sg.genre_id
+FROM tv_shows AS s
+LEFT JOIN tv_show_genres AS sg ON s.id = sg.show_id
+LEFT JOIN tv_genres AS g ON g.id = sg.genre_id
 ORDER BY s.title ASC, g.name ASC;
