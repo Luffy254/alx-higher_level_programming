c07b5b8e0c5c64004d9dd5c8b27e57b2dbbc1835
Samuel Kiarie <87488083+Luffy254@users.noreply.github.com>
2023-10-25 20:34:33 +0300

    Delete 0x0E-SQL_more_queries directory

--- 0x0E-SQL_more_queries/102-rating_shows.sql
+++
@@ -1,7 +0,0 @@
--- list all shows from hbtn_0d_tvshows_rate by their rating.
-SELECT s.title AS Show_Title,
-       SUM(r.rate) AS Show_Rating
-  FROM tv_shows s
-       JOIN tv_show_ratings r ON s.id = r.show_id
- GROUP BY Show_Title
- ORDER BY Show_Rating DESC;
